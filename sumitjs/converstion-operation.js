2fea6ab33bfe1ff1a167920a123a26e32bbd9505
sidsumitgupta <750sumitgupta@gmail.com>
2024-11-10 15:19:02 +0000

    converstion-operation

---
+++ sumitjs/converstion-operation.js
@@ -0,0 +1,21 @@
+//in this we convert one from to other form datatypes
+// for example
+let score = 70
+let stringno = String(score)
+console.log(stringno);
+//from here we can our value is store in new variable
+// now check new variable datatypes 
+console.log(typeof stringno);
+// it work now check older variable datatypes 
+console.log(typeof score);
+//as intinally score is number we can see 
+
+let nameIA = "sumit"
+let numerIs = Number(nameIA)
+ console.log(typeof numerIs);
+ console.log(numerIs);
+ 
+ // we can change any datatypes into any datatypes 
+ // a string is not convert into number => NaN
+ //same for boolean func also
+
