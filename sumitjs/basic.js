03c26a10c94b8b9bbf1dbcc5091431b80b7ee779
sidsumitgupta <750sumitgupta@gmail.com>
2024-11-10 14:34:29 +0000

    datatypes

--- sumitjs/basic.js
+++ sumitjs/basic.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const accountId = 14435
+let accountEmail = "sumitgupta@gamil.com"
+var accountPassword = "1234"
+accountCity = "GKP"
+
+//accountId = 2 not allowed
+accountEmail = "sum@hc.com"
+accountPassword = "123432"
+accountCity = "lucknow"
+
+/* not use var 
+  buz issue in block scope & func issue
+  */
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+
---
+++ sumitjs/datatypes.js
@@ -0,0 +1,16 @@
+// alert(3+3) not  working buz using nodejs not browser//
+console.log(3+3);
+
+
+// number=> 2 ka power 53 
+// bigint
+//string=> ""
+// boolearn=> true/false
+//null=> standalone value ek object h 
+// undefine => value nhi diya gya h 
+// symbol=> uniqueness ke liye 
+
+let name = "sumit"
+let age = 18
+
+console.log(typeof age);
