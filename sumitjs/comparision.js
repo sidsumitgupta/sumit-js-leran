2cacc3eb7bb43b2f500614166e0737b68dd53212
sidsumitgupta <750sumitgupta@gmail.com>
2024-11-11 18:12:03 +0000

    comparision

---
+++ sumitjs/comparision.js
@@ -0,0 +1,20 @@
+ /* console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1); */
+// this are basic types of comparision
+
+console.log("1" > 3);
+console.log("2" > 1);
+
+// here its convert string types into number for converstion
+
+// === three equal to mean it also check datatypes
+console.log("2"===2);
+// ans is flase buz of datatypes 
+
+
+
+
+
---
+++ sumitjs/operation.js
@@ -0,0 +1,38 @@
+let value = 5
+let negvalue = -value
+console.log(negvalue);
+// we sign -ve value & and got -ve value
+ 
+// eg of basic opertaion 
+//console.log(2+2);
+//console.log(2*2);
+/*console.log(2**3); it mean 2 ka power 3 
+console.log(2/3);  it optain divide
+console.log(2%3); remainder optain
+*/
+// adding of two string
+ let str1 = "hello"
+ let str2 = " hey"
+
+ let str3 = str1+str2
+ console.log(str3);
+ // if string first thrn all treat as string 
+ // if no is first then first operation perform then same as string eg are 
+ 
+ console.log("1"+6 + 5);
+ console.log(1+2+"4");
+ 
+ 
+let x = 3
+const y = ++x;
+console.log(y ,x);
+
+// here we can see postfix(x++) is return the value as it is then increment the value
+// for prefix (++x) is first increment the value then return it
+
+
+
+
+
+
+
