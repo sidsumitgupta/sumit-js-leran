0ee58b570e82994bbee16a3659c2476a46a1660f
sidsumitgupta <750sumitgupta@gmail.com>
2024-11-22 18:32:03 +0000

    02loops

---
+++ loops/02.js
@@ -0,0 +1,17 @@
+// for of 
+
+//["","",]
+//[{},{},{}]
+
+const arr = [1,2,4,7,8,]
+for (const num of arr) {
+    console.log(num);  
+}
+
+const say = "hello"
+for (const hey of say) {
+ console.log(`Each char is ${say}`);
+}
+
+//maps 
+
